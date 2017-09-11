9cb0758930c4bbd96eaa41603922dfc01875c8df
jennadaly <jenna.m.daly@gmail.com>
2017-09-11 17:07:48 -0400

    Initial commit, DP, and testing complete.

---
+++ scripts/getData.R
@@ -0,0 +1,90 @@
+library(acs)
+source('./scripts/acsHelpers.R')
+
+# ACS B23008
+# Get geography object for CT and subcounty divisions
+acsdata <- getACSData(
+  getCTGeos("town"),
+  yearList = 2010:2015,
+  get_this_one = "B11003"
+)
+
+get_data <- data.table()
+for (data in acsdata) {
+  year <- as.numeric(data@endyear)
+  year <- paste(year-4, year, sep="-")
+  
+  #based on the VD0x <- the x is what you want as the range, signifies which columns you want to sum
+  total_families <- acsSum(data, 1, "Total Families") 
+  total_families_own_children <- acsSum(data, c(3,10,16), "Total Families with Children of Their Own") 
+  total_families_no_own_children <- acsSum(data, c(7,14,20), "Total Families without Children of Their Own") 
+  total_single_parent_families <- acsSum(data, 8, "Total Single-Parent Families")
+  total_single_parent_own_children <- acsSum(data, c(10,16), "Total Single-Parent Families with Children of Their Own") 
+  total_single_parent_no_own_children <- acsSum(data, c(14,20), "Total Single-Parent Families without Children of Their Own") 
+
+  datafips <- data.table(fips = getACSFips(data))
+  
+  estimates <- data.table(
+    FIPS = datafips$fips,
+    Year = year,
+    estimate(total_families),
+    estimate(total_families_own_children),
+    estimate(total_families_no_own_children),
+    estimate(total_single_parent_families),
+    estimate(total_single_parent_own_children),
+    estimate(total_single_parent_no_own_children)
+  )
+  
+  names(estimates)[names(estimates) == "HD01_VD01.Estimate; Total:"] <- "Total Families"
+  names(estimates)[names(estimates) == "HD01_VD08.Estimate; Other family:"] <- "Total Single-Parent Families"
+  
+  estimates <- melt(
+    estimates,
+    id.vars = c("FIPS", "Year"),
+    variable.name = "Group",
+    variable.factor = F,
+    value.name = "Value",
+    value.factor = F
+  )
+  
+  moes <- data.table(
+    FIPS = datafips$fips,
+    Year = year,
+    #multiplying by 1.645 assigns confidence level to 90%
+    # SE = MOE / 1.645
+    standard.error(total_families) * 1.645,
+    standard.error(total_families_own_children) * 1.645,
+    standard.error(total_families_no_own_children) * 1.645,
+    standard.error(total_single_parent_families) * 1.645,
+    standard.error(total_single_parent_own_children) * 1.645,
+    standard.error(total_single_parent_no_own_children) * 1.645
+  )
+  
+  names(moes)[names(moes) == "HD01_VD01.Estimate; Total:"] <- "Total Families"
+  names(moes)[names(moes) == "HD01_VD08.Estimate; Other family:"] <- "Total Single-Parent Families"
+  
+  
+  moes <- melt(
+    moes,
+    id.vars = c("FIPS", "Year"),
+    variable.name = "Group",
+    variable.factor = F,
+    value.name = "MOE",
+    value.factor = F
+  )
+  
+  setkey(estimates, FIPS, Year, Group)
+  setkey(moes, FIPS, Year, Group)
+  
+  get_data <- rbind(get_data, estimates[moes])
+}
+
+get_data <- get_data[get_data$FIPS != "0900100000",]
+
+# Write to File
+write.table(
+  get_data,
+  file.path(getwd(), "raw", "group_totals.csv"),
+  sep = ",",
+  row.names = F
+)
---
+++ scripts/single-parent-families-processing.R
@@ -0,0 +1,111 @@
+library(dplyr)
+library(datapkg)
+library(tidyr)
+
+##################################################################
+#
+# Processing Script for Single Parent Families
+# Created by Jenna Daly
+# On 09/11/2017
+#
+##################################################################
+
+source('./scripts/getData.R')
+
+#Setup environment
+sub_folders <- list.files()
+raw_location <- grep("raw", sub_folders, value=T)
+path_to_raw <- (paste0(getwd(), "/", raw_location))
+
+#Merge in FIPS
+town_fips_dp_URL <- 'https://raw.githubusercontent.com/CT-Data-Collaborative/ct-town-list/master/datapackage.json'
+town_fips_dp <- datapkg_read(path = town_fips_dp_URL)
+fips <- (town_fips_dp$data[[1]])
+
+sp_families <- merge(get_data, fips, by = "FIPS", all.y=T)
+
+#Convert from long to wide to calculate rates
+sp_families_value <- sp_families %>% select(-MOE)
+sp_families_value <- spread(sp_families_value, Group, Value)
+
+sp_families_moe <- sp_families %>% select(-Value)
+sp_families_moe <- spread(sp_families_moe, Group, MOE)
+
+#Append either Value or MOE to columns so we can merge columns
+names(sp_families_value)[4:9] <- gsub("$", " Value", names(sp_families_value)[4:9])
+names(sp_families_moe)[4:9] <- gsub("$", " MOE", names(sp_families_moe)[4:9])
+
+sp_families_wide <- merge(sp_families_value, sp_families_moe, by = c("FIPS", "Year", "Town"))
+
+#############################################################################################
+# Helper function for MOE
+calcMOE <- function(x, y, moex, moey) {
+  moex2 <- moex^2
+  moey2 <- moey^2
+  d <- x/y
+  d2 <- d^2
+  
+  radicand <- ifelse(
+    moex2 < (d2 * moey2),
+    moex2 + (d2 * moey2),
+    moex2 - (d2 * moey2)
+  )
+  
+  return(sqrt(radicand)/y)
+}
+
+# calculate group rates with total denominators,
+# keep MOES, calculating appropriately
+sp_families_wide_calc <- sp_families_wide %>%
+  mutate(`% Families, Single-parent` = ((`Total Single-Parent Families Value` / `Total Families Value`)*100),
+         `MOE of % Families, Single-parent` = (calcMOE(`Total Single-Parent Families Value`, 
+                                                       `Total Families Value`, 
+                                                       `Total Single-Parent Families MOE`, 
+                                                       `Total Families MOE`)),
+         `% Families, Single-parent, without Children of Their Own` = ((`Total Single-Parent Families without Children of Their Own Value` / `Total Families Value`)*100),  
+         `MOE of % Families, Single-parent, without Children of Their Own` = (calcMOE(`Total Single-Parent Families without Children of Their Own Value`, 
+                                                                                      `Total Families Value`, 
+                                                                                      `Total Single-Parent Families without Children of Their Own MOE`, 
+                                                                                      `Total Families MOE`)),
+  
+         `% Families, Single-parent, with Children of Their Own` = ((`Total Single-Parent Families with Children of Their Own Value` / `Total Families Value`)*100),
+         `MOE of % Families, Single-parent, with Children of Their Own` = (calcMOE(`Total Single-Parent Families with Children of Their Own Value`, 
+                                                                     `Total Families Value`, 
+                                                                     `Total Single-Parent Families with Children of Their Own MOE`, 
+                                                                     `Total Families MOE`)))
+options(scipen=999)
+
+sp_families_long <- gather(sp_families_wide_calc, Group, Value, 4:21, factor_key = FALSE)
+
+sp_families_long$`Measure Type` <- "Number"
+sp_families_long$`Measure Type`[which(grepl("%", sp_families_long$Group))] <- "Percent"
+
+sp_families_long$Variable <- "Single-Parent Families"
+sp_families_long$Variable[which(grepl("MOE", sp_families_long$Group))] <- "Margins of Error"
+
+sp_families_long$`Family Type` <- "All"
+sp_families_long$`Family Type`[which(grepl("Single", sp_families_long$Group))] <- "Single-Parent"
+
+sp_families_long$`Child Relation` <- "All"
+sp_families_long$`Child Relation`[which(grepl("without", sp_families_long$Group))] <- "No Own Children"
+sp_families_long$`Child Relation`[which(grepl("with ", sp_families_long$Group))] <- "Own Children"
+
+sp_families_long$Group <- NULL
+
+sp_families_long$Value <- round(sp_families_long$Value, 2)
+
+sp_families_long <- sp_families_long %>% 
+  select(Town, FIPS, Year, `Family Type`, `Child Relation`, `Measure Type`, Variable, Value) %>% 
+  arrange(Town, Year, `Measure Type`, Variable, `Family Type`, `Child Relation`)
+
+# Write to File
+write.table(
+  sp_families_long,
+  file.path(getwd(), "data", "single_parent_families_2015.csv"),
+  sep = ",",
+  row.names = F
+)
+
+
+
+
